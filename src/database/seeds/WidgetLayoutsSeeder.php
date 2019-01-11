0205c35e87fd6020dd46e7a268078141eb99fbda
Aleksander <32997216+Aleksandr1705@users.noreply.github.com>
2019-01-11 10:04:37 +0300

    Update WidgetLayoutsSeeder.php

--- src/database/seeds/WidgetLayoutsSeeder.php
+++ src/database/seeds/WidgetLayoutsSeeder.php
@@ -17,31 +17,38 @@ class WidgetLayoutsSeeder extends Seeder
 
         \DB::table('widgetlayouts')->insert([
              [
-                'name' => 'Разметка 4_4_4',
+                'name' => 'Layout 4_4_4',
                 'fname'=>'layout_4_4_4'
             ],
                [
-                'name' => 'Разметка 2_5_5',
+                'name' => 'Layout 2_5_5',
                 'fname'=>'layout_2_5_5'
             ],[
-                'name' => 'Разметка 5_5_2',
+                'name' => 'Layout 5_5_2',
                 'fname'=>'layout_5_5_2'
             ],[
-                'name' => 'Разметка 7_5',
+                'name' => 'Layout 7_5',
                 'fname'=>'layout_7_5'
             ],[
-                'name' => 'Разметка 5_7',
+                'name' => 'Layout 5_7',
                 'fname'=>'layout_5_7'
             ],[
-                'name' => 'Разметка 4_8',
+                'name' => 'Layout 4_8',
                 'fname'=>'layout_4_8'
             ],[
-                'name' => 'Разметка 8_4',
+                'name' => 'Layout 8_4',
                 'fname'=>'layout_8_4'
             ],[
-                'name' => 'Разметка 6_6',
+                'name' => 'Layout 6_6',
                 'fname'=>'layout_6_6'
             ], 
+        ]);
+        \DB::table('widgetboards')->delete();
+        \DB::table('widgetboards')->insert([
+            [
+                'name'=>'System dashboard',
+                'widgetlayout_id'=>1
+            ]
         ]);
     }
 }
