58e000894d85993460bcf40d76a39e1e4ed0d4ed
Aleksandr1705 <32997216+Aleksandr1705@users.noreply.github.com>
2019-01-13 15:33:59 +0300

    Исправлен путь к файлам миграции БД

--- src/app/Http/Controllers/Admin/WidgetBodyController.php
+++ src/app/Http/Controllers/Admin/WidgetBodyController.php
@@ -10,12 +10,17 @@ class WidgetBodyController extends Controller
 {
     //
     public function getBody(Widget $widget){
+        $body="";
         switch($widget->id){
             case 0:
                 break;
             case 1:
+                $body="w1";
+                break;
+            case 2:
+                $body="w2";
                 break;
         }
-        return "body";
+        return $body;
     }
 }
--- src/resources/views/widget/widget.blade.php
+++ src/resources/views/widget/widget.blade.php
@@ -1,4 +1,4 @@
-<div class = "box box-default" data-widget='box-refresh' data-source='widgetbody/{{ $widget->id }}'>
+<div class = "box box-default" data-widget="box-refresh" data-source="widgetbody/{{ $widget->id }}">
     <div class = "box-header with-border">
         <div class = "box-title">{{ $widget->descr }}</div>
     </div>
