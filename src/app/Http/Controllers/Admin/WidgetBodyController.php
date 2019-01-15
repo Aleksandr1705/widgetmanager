5f4387f4c848cd403ece97b3e1134093eee352cc
Aleksandr1705 <32997216+Aleksandr1705@users.noreply.github.com>
2019-01-15 19:42:51 +0300

    Обновлен WidgetBodyController

--- src/app/Http/Controllers/Admin/WidgetBodyController.php
+++ src/app/Http/Controllers/Admin/WidgetBodyController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace almosoft\widgetmanager\app\Http\Controllers\Admin;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use almosoft\widgetmanager\app\Http\Controllers\Admin\WidgetBaseController;
